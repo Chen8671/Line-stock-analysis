863b9698deb535052b1f5f223d08005c7a6aecca
oag <100293071+Chen8671@users.noreply.github.com>
2025-01-16 01:29:25 +0800

    Update linebotA1.py

--- linebotA1.py
+++ linebotA1.py
@@ -9,8 +9,8 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage, TemplateS
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.environ.get('mXE1BzBQ67nBGrZGbBO0TEWrT3xy9h3rpk4sz+PGeC00bwwc3yvWz9BEANYMNpm0MqpSk7xfmEh6l2KEy/KFEAduvGPm3m7A++Sxl3eJTiSzeQlzZJhxXfDoiyEdfGnsDern1toKbzLJdDe/IvtFpwdB04t89/1O/w1cDnyilFU='))
-handler = WebhookHandler(os.environ.get('7c7b7ddfcfa323b252f5f4d81a4bff1d'))
+line_bot_api = LineBotApi(os.environ.get('LINE_CHANNEL_ACCESS_TOKEN'))
+handler = WebhookHandler(os.environ.get('LINE_CHANNEL_SECRET'))
 
 # 查詢股票健康狀況的函數
 def get_stock_health(stock_code):
